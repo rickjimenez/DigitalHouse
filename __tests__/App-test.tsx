c06803e3f051b2814d36cff1e0e5cea4ffdd39f2
Rick Jimenez <ricardo.jimenez@tribal.credit>
2022-12-19 11:06:39 -0600

    init app

---
+++ __tests__/App-test.tsx
@@ -0,0 +1,15 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import {render, screen} from '@testing-library/react-native';
+import React from 'react';
+import App from '../App';
+
+test('renders correctly', () => {
+  render(<App />);
+
+  const app = screen.toJSON();
+  expect(app).toMatchSnapshot();
+});
